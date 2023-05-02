0655a1339f5088270f50a6c1c0ca53b99209e96d
Diego Castro <diego.castro@potros.itson.edu.mx>
2023-05-02 02:17:26 -0700

    Documentacion del proyecto

--- app/src/main/java/mx/itson/sol/entidades/Ubicacion.kt
+++ app/src/main/java/mx/itson/sol/entidades/Ubicacion.kt
@@ -2,12 +2,32 @@ package mx.itson.sol.entidades
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Variables que se obtienen del JSON de OpenMeteo
+ *
+ * @constructor Crea una clase para obtener las variables del JSON de OpenMeteo
+ * @author Diego Castro Arce
+ */
 class Ubicacion {
 
+    /**
+     * Latitud de la ubicacion
+     */
     var latitude : Float? = null
+
+    /**
+     * Longitud de la ubicacion
+     */
     var longitude: Float? = null
+
+    /**
+     * Elevacion de la ubicacion
+     */
     var elevation:Float? = null
 
+    /**
+     * Objeto clima para obtener las variables del clima actual
+     */
     @SerializedName("current_weather")
     var clima:Clima? = null
 
--- app/src/main/java/mx/itson/sol/interfaces/OpenMeteo.kt
+++ app/src/main/java/mx/itson/sol/interfaces/OpenMeteo.kt
@@ -5,8 +5,22 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+/**
+ * Interface para obtener el clima con OpenMeteo
+ *
+ * @constructor Crea una interface para configurar los parametros de la peticion
+ * @author Diego Castro Arce
+ */
 interface OpenMeteo {
 
+    /**
+     * Obtiene el clima de una ubicacion
+     *
+     * @param lat Latitud de la ubicacion
+     * @param lon Longitud de la ubicacion
+     * @param current Si es el clima actual
+     * @return Call<Ubicacion> Llamada para obtener el clima
+     */
     @GET("forecast")
     fun getClima(@Query("latitude") lat:Double,
                  @Query("longitude") lon:Double,
